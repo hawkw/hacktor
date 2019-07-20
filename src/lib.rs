abdb89070158edd486a59e4a26258b2e59534258
Eliza Weisman <eliza@buoyant.io>
2019-07-20 08:56:53 -0700

    naming
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- src/lib.rs
+++ src/lib.rs
@@ -28,12 +28,12 @@ pub trait Process<M: Message> {
     fn poll_ready(&mut self, ctx: &mut Context<'_>) -> Poll<Result<(), Self::Error>>;
 
     /// Handles a message, returning a future.
-    fn handle(&mut self, message: M, ctx: &mut Context<'_>) -> Self::Future;
+    fn recv(&mut self, message: M, ctx: &mut Context<'_>) -> Self::Future;
 }
 
 /// A reference to a local or remote actor.
 pub trait Ref<A> {
-    fn send<M>(&mut self, msg: M) -> SendFuture<<A as Process<M>>::Response, <A as Process<M>::Error>>
+    fn send<M>(&mut self, msg: M) -> SendFuture<A, M>
     where
         A: Process<M>,
         M: Message;
