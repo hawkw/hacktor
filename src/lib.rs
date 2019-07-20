7ead57e88f1a321b874d6809164cd780d3c7cffa
Eliza Weisman <eliza@buoyant.io>
2019-07-20 08:41:59 -0700

    message trait?
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,20 @@ use std::{
     task::{self, Poll},
 };
 
+use serde::{Serialize, Deserialize};
+
 pub struct Context<'a> {
     // used to wake the actor's task.
     task: task::Context<'a>,
     // ...
 }
 
+/// A message that can be sent between threads, over IPC, or across the network.
+pub trait Message: Serialize + for<'de> Deserialize<'de> + Send {}
+
 /// This is the actor trait, but I wasn't sure if it ought to be called that.
-pub trait Process<M> {
-    type Yield;
+pub trait Process<M: Message> {
+    type Yield: Message;
     type Error;
     type Future: Future<Output = Result<Self::Yield, Self::Error>>;
 
@@ -29,10 +34,14 @@ pub trait Process<M> {
 pub trait Ref<A> {
     fn send<M>(&mut self, msg: M) -> SendFuture<<A as Process<M>>::Yield, <A as Process<M>::Error>>
     where
-        A: Process<M>;
+        A: Process<M>,
+        M: Message;
 }
 
 pub struct SendFuture<I, E> {
     // TODO
     _p: std::marker::PhantomData<(I, E)>,
 }
+
+
+impl<M> Message for M where M: Serialize + for<'de> Deserialize<'de> + Send {}
